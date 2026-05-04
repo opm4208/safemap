a7c6f068893ac6778c63d695186c3af82f93f0ea
opm4208 <108387647+opm4208@users.noreply.github.com>
2026-05-04 16:50:53 +0900

    fix app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+// App 컴포넌트가 정상적으로 렌더링되는지 확인
+test("renders app without crashing", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
